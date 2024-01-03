2017571e9506dc0b8b66cc4c10eb29ace06f3f6d
Alexanderk30 <alexanderk30@icloud.com>
2024-01-03 02:02:05 -0500

    worked on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
+import ButtonAppBar from './components/appBar';
+
+import { ButtonBase } from '@mui/material';
 
 function App() {
+
   return (
     <div className="App">
- {/* everything we code will be in here */}
+    {/* everything we code will be in here */}
+      
+      <div id='LandingPageLargeOpening'>
+        <ButtonAppBar/> 
+      </div>
+      <div id='LandingPageExampleSchedules'>
+        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
+        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
+        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
 
+      </div>
     </div>
   );
 }
---
+++ src/components/appBar.tsx
@@ -0,0 +1,49 @@
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
+
+
+export default function ButtonAppBar() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  function close () {
+    setDrawerOpen(false);
+  }
+
+  function open() {
+    setDrawerOpen(true);
+  }
+
+    return (
+        <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={open}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+             null
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer
+      anchor={"left"}
+      open={drawerOpen}
+      onClose={close}
+      
+    >
+      <p>sdsdsd</p>
+    </Drawer>
+    </>
+    );
+  }
