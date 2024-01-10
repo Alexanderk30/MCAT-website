93454ed17b83626171a6ce079483ac55e39677a9
Alexanderk30 <alexanderk30@icloud.com>
2024-01-09 20:45:41 -0500

    Added images to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import ButtonAppBar from './components/appBar';
-
+import imageFarLeft from "./assets/img/undraw_coding_re_iv62.svg";
+import middleImage from "./assets/img/undraw_in_progress_re_m1l6.svg";
+import farRight from "./assets/img/undraw_lightbulb_moment_re_ulyo (1).svg";
 import { ButtonBase } from '@mui/material';
 
 function App() {
@@ -13,10 +15,10 @@ function App() {
         <ButtonAppBar/> 
       </div>
       <div id='LandingPageExampleSchedules'>
-        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
-        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
-        <ButtonBase className='ExampleSchedule'>asd</ButtonBase>
-
+        <ButtonBase className='ExampleSchedule'> <img src={imageFarLeft}/></ButtonBase>
+        <ButtonBase className='ExampleSchedule'><img src={middleImage}/></ButtonBase>
+        <ButtonBase className='ExampleSchedule'><img src={farRight}/></ButtonBase>
+        <ButtonBase className='TryExampleSchedules'>Try These Example Schedules</ButtonBase>
       </div>
     </div>
   );
