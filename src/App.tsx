44062adb030a20db20b13d4784dcfb23f391f23f
Alexanderk30 <alexanderk30@icloud.com>
2024-01-11 21:53:24 -0500

    Finished main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import imageFarLeft from "./assets/img/undraw_coding_re_iv62.svg";
 import middleImage from "./assets/img/undraw_in_progress_re_m1l6.svg";
 import farRight from "./assets/img/undraw_lightbulb_moment_re_ulyo (1).svg";
 import { ButtonBase } from '@mui/material';
+import MainPageCalender from "./assets/img/MCATwebsiteMainPageBackground.jpg";
 // import HomepageCalender from"./assets/img/
 
 function App() {
@@ -13,11 +14,9 @@ function App() {
     {/* everything we code will be in here */}
       
       <div id='LandingPageLargeOpening'>
-        <ButtonAppBar/> 
+        <ButtonAppBar/> <img src={MainPageCalender}/>
       </div>
-      <img src="MCATwebsiteMainPageBackground.jpg" width="20px" height="20px">
       
-      </img>
       <div id='WebsiteDescription'>
         <h1>mcatPlanner</h1>
           <p>Welcome to the MCAT Study Plan Creator – Your Gateway to Success! 🚀Embark on your journey to conquer the MCAT with confidence using our dynamic study plan generator. Tailored to accommodate your unique learning style and schedule, our innovative tool crafts a personalized roadmap that optimizes your preparation for this critical examination.</p>
@@ -27,7 +26,7 @@ function App() {
         <ButtonBase className='ExampleSchedule'> <img src={imageFarLeft}/></ButtonBase>
         <ButtonBase className='ExampleSchedule'><img src={middleImage}/></ButtonBase>
         <ButtonBase className='ExampleSchedule'><img src={farRight}/></ButtonBase>
-        <ButtonBase className='TryExampleSchedules'>Try These Example Schedules</ButtonBase>
+        <ButtonBase className='TryExampleSchedules'>Click The Icons To Try These Example Schedules</ButtonBase>
       </div>
     
     </div>
