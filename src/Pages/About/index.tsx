23aa14eda0b00df9e497a2b864b360310f9cf33d
Alexanderk30 <alexanderk30@icloud.com>
2024-01-16 12:09:29 -0500

    Added about page setup

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -1,14 +1,37 @@
+import './About.css'
 import { Link } from "react-router-dom";
-
+import ButtonAppBar from '../../components/appBar';
 export default function AboutPage() {
   return (
     <div id="AboutPage">
-      <h2>About Page</h2>
-      <p>
-        Notice the url has been changed. Try refreshing the page see what
-        happens.
-      </p>
-      <Link to="/">home</Link>
+       <ButtonAppBar/>
+    <div id="Title">
+      <h1>Why Use MCATPlanner?</h1>
+      </div>
+    <div id="one">
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida sollicitudin ipsum. Quisque euismod aliquet bibendum. Cras faucibus vehicula dictum. Vivamus accumsan vel purus sit amet laoreet. Ut vestibulum at dui sit amet tincidunt. Sed vitae odio vitae mauris ultricies bibendum. Nulla vitae nulla non tortor auctor egestas vel vitae augue. Nunc rutrum ipsum orci, id aliquet arcu lobortis sit amet. Maecenas sed commodo libero. Nullam suscipit velit mi, vel rutrum sem aliquam in.
+    </p>
+    </div>
+    <div id="two">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida sollicitudin ipsum. Quisque euismod aliquet bibendum. Cras faucibus vehicula dictum. Vivamus accumsan vel purus sit amet laoreet. Ut vestibulum at dui sit amet tincidunt. Sed vitae odio vitae mauris ultricies bibendum. Nulla vitae nulla non tortor auctor egestas vel vitae augue. Nunc rutrum ipsum orci, id aliquet arcu lobortis sit amet. Maecenas sed commodo libero. Nullam suscipit velit mi, vel rutrum sem aliquam in.
+</p>
+    </div>
+    <div>
+    <div id="three">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida sollicitudin ipsum. Quisque euismod aliquet bibendum. Cras faucibus vehicula dictum. Vivamus accumsan vel purus sit amet laoreet. Ut vestibulum at dui sit amet tincidunt. Sed vitae odio vitae mauris ultricies bibendum. Nulla vitae nulla non tortor auctor egestas vel vitae augue. Nunc rutrum ipsum orci, id aliquet arcu lobortis sit amet. Maecenas sed commodo libero. Nullam suscipit velit mi, vel rutrum sem aliquam in.
+</p>
+    </div>
+    <div id="four">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida sollicitudin ipsum. Quisque euismod aliquet bibendum. Cras faucibus vehicula dictum. Vivamus accumsan vel purus sit amet laoreet. Ut vestibulum at dui sit amet tincidunt. Sed vitae odio vitae mauris ultricies bibendum. Nulla vitae nulla non tortor auctor egestas vel vitae augue. Nunc rutrum ipsum orci, id aliquet arcu lobortis sit amet. Maecenas sed commodo libero. Nullam suscipit velit mi, vel rutrum sem aliquam in.
+</p>
+    </div>
+    <div id="five">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam gravida sollicitudin ipsum. Quisque euismod aliquet bibendum. Cras faucibus vehicula dictum. Vivamus accumsan vel purus sit amet laoreet. Ut vestibulum at dui sit amet tincidunt. Sed vitae odio vitae mauris ultricies bibendum. Nulla vitae nulla non tortor auctor egestas vel vitae augue. Nunc rutrum ipsum orci, id aliquet arcu lobortis sit amet. Maecenas sed commodo libero. Nullam suscipit velit mi, vel rutrum sem aliquam in.
+</p>
+    </div>
+    <Link to="/">home</Link>
+    </div>
     </div>
   );
 }
